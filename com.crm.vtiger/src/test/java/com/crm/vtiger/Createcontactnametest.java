c766ea707e8823de23ee6c029d60885727128c17
Kishorkumar <Kishorkumar@Lenovo-PC>
2022-03-24 15:09:13 +0530

    pushed

---
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/Createcontactnametest.java
@@ -0,0 +1,12 @@
+package com.crm.vtiger;
+
+import org.testng.annotations.Test;
+
+public class Createcontactnametest {
+	@Test
+	public void contactname()
+	{
+		System.out.println("contactname created successfully");
+	}
+
+}
